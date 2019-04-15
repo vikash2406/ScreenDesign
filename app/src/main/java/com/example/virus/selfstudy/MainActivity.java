ab3c4cb645dc071dae682ecb07b8d8a9eea48c33
vikash2406 <kumarvikashrocks1997@gmail.com>
2019-04-15 23:48:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/virus/selfstudy/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.virus.selfstudy;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.icon , menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
